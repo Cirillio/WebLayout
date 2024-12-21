6e8d894337225df3e48d0780056b51638cf8cce3
Cirillio <ineed2healing@gmail.com>
2024-12-22 02:02:33 +0700

    fix github deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./dist/*.html"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     extend: {
       fontFamily: { Bebas: ["Bebas Neue", "cursive"], Poppins: ["Poppins"] },
