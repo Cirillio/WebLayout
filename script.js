cbd8fd4fbab291ff7b37111a6c50a3da2e4abaec
Cirillio <ineed2healing@gmail.com>
2024-12-22 01:55:49 +0700

    connected Swiper, slider works.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-`use strict`;
+import Swiper from "./node_modules/swiper/swiper-bundle.min.mjs"; // Инициализация Swiper
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1, // Один слайд на экране
+  loop: false, // Выключаем бесконечный цикл
+});
 
-document.addEventListener("DOMContentLoaded", () => {
-  const headerColorToggles = document.querySelectorAll(".header-color-toggles");
-  headerColorToggles.forEach((toggle) => {
-    toggle.addEventListener("click", () => {
-      headerColorToggles.forEach((tgl) => {
-        tgl.classList.remove("header-color-toggles-checked");
-      });
-      toggle.classList.add("header-color-toggles-checked");
-    });
+// Находим все кнопки
+const buttons = document.querySelectorAll("#main_sneakers_toggles button");
+
+// Добавляем обработчики событий на кнопки
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    // Переключаем на соответствующий слайд
+    swiper.slideTo(index);
+
+    // Обновляем классы кнопок
+    buttons.forEach((btn) =>
+      btn.classList.remove("header-color-toggles-checked")
+    );
+    button.classList.add("header-color-toggles-checked");
   });
+});
+
+swiper.on("slideChange", () => {
+  buttons.forEach((btn) =>
+    btn.classList.remove("header-color-toggles-checked")
+  );
+  buttons[swiper.activeIndex].classList.add("header-color-toggles-checked");
 });
